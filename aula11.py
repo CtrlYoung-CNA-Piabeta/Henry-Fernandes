1cf6d661e1fcffeed570ad4e144528b9c8cee0f6
Henry-Fernandes <henry.minecraft09@gmail.com>
2024-06-10 19:57:51 -0300

    media dos numeros de uma lista

--- aula11.py
+++ aula11.py
@@ -1,2 +1,10 @@
 # Calcular a média em uma lista
 # Ex: [1, 2, 3, 4, 5] = 3
+
+def media(lista):
+    soma = 0
+    for num in lista:
+        soma += num
+    return soma / len(lista)
+lista1 = [1 ,2 ,3 ,4 ,5 ,6 ,7 ,8 , 9, 10]
+print(media(lista1))
