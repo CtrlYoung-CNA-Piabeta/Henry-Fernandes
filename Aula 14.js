e119f9c5e21ee6ce6492ca9c7f36dc7c5b2ce4ee
Henry_Fernandes <henry.minecraft09@gmail.com>
2024-11-11 20:01:03 -0300

    aula13

---
+++ Aula 14.js
@@ -0,0 +1,39 @@
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// function soma(a , b){
+//     console.log(a + b)
+//     return a + b
+// }
+
+// function subtr(a , b){
+//     console.log(a - b)
+//     return a - b
+// }
+
+// function multipl(a , b){
+//     console.log(a * b)
+//     return a * b
+// }
+
+// function divis(a , b){
+//     console.log(a / b)
+//     return a / b
+// }
+
+// function contagem(){
+//     for(let i = 1; i <= 10; i++){
+//         console.log(i)
+//     }
+// }
+
+lista = [1, 5, "2", "7", 2]
+        
+function somatudo(){
+    for(let i = 0; i <= lista.length; i++){
+        return Number(i)
+        
+    }
+    
+}
